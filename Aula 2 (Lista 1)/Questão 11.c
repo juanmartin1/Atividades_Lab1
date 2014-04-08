da9b8ef5035ac28d74af9e8e593920b0de497b7e
Juan Martin Leroux <juan_martin_213@hotmail.com>
2014-04-08 01:16:03 -0300

    QUestão 11

---
+++ Aula 2 (Lista 1)/Questão 11.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+	int l;
+	scanf("%d",&l);
+	printf("A area do quadrado de lado %d e %d\n", l, l*l);
+	printf("O dobro da area do quadrado e %d", l*l*2);
+	return(0);
+}
