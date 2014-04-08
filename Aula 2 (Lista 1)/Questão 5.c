8c262f2b68392540a9d15a2994fea6236dada29b
Juan Martin Leroux <juan_martin_213@hotmail.com>
2014-04-08 00:09:25 -0300

    Questão 5

--- Aula 2 (Lista 1)/Questão 5.c
+++ Aula 2 (Lista 1)/Questão 5.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main(){
-	char c1, c2;
-	scanf("%c", &c1);
-	scanf("%c", &c2);
-	printf("Voce digitou os strings: %c, %c", c1,c2);
+	char c1[20], c2[20];
+	scanf("%s", &c1);
+	scanf("%s", &c2);
+	printf("Voce digitou os strings: %s, %s\n", c1, c2);
+	printf("A segunda letra das duas palavras e respectivamente: %c, %c", c1[1], c2[1]);
 	return 0;
-	getchar();
 }
