6b272b2e2de4651449df6c62714d9c9b9c305cb6
juanmartin213 <juan_martin_213@hotmail.com>
2014-06-08 17:26:41 -0300

    Update Questão 20.cpp

--- Aula 2 (Lista 1)/Questão 20.cpp
+++ Aula 2 (Lista 1)/Questão 20.cpp
@@ -2,7 +2,10 @@
 
 int main(){
 	float Mb, Mbps;
-	scanf ("%f %f", &Mb, &Mbps);
-	printf("O tempo de download e: %fmin", Mb/Mbps/60.0);	
+	printf("Digite o tamanho do arquivo (em MB)\n");
+  	scanf("%f", &Mb);
+  	printf("Digite a velocidade de download (em Mbps)\n");
+  	scanf("%f", &Mbps);
+	printf("O tempo de download e: %.3fmin", Mb/Mbps/60.0);	
 	return 0;
 }
