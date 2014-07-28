b35894aedf6a752ae9ce5231c67cab3cff7e7193
juanmartin213 <juan_martin_213@hotmail.com>
2014-07-28 00:22:03 -0300

    Lista6

---
+++ Lista6-ifndef-QUEST1_H-define-QUEST1_H-include-stdio.h-include-stdlib.h/Questão 3.h
@@ -0,0 +1,12 @@
+#ifndef QUEST1_H
+#define QUEST1_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int tamanhoarq(FILE *pFile);
+
+void read(FILE *pFile1, FILE *pFile2, char* c1, char* c2);
+
+#endif
