6789aa44d85aa67d8ecb2402d95238fb2289968b
juanmartin213 <juan_martin_213@hotmail.com>
2014-07-28 00:22:23 -0300

    Lista6

---
+++ Lista6-ifndef-QUEST1_H-define-QUEST1_H-include-stdio.h-include-stdlib.h/Questão 4.h
@@ -0,0 +1,13 @@
+#ifndef QUEST4_H
+#define QUEST4_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//Lê do teclado duas strings.
+void ler2str(char* ste1, char* str2);
+
+//muda as letras do string de minuscula para maiusculas.
+void m_to_M(char* c1);
+
+#endif
